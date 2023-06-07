6ee2eb712dd95bad38fb2d94346d4740b33fe71f
Francisco Quiroz <134228349+FranciscoQuirozAllende@users.noreply.github.com>
2023-06-07 00:54:55 -0400

    me puse wn sry :c

--- inicio.py
+++ inicio.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, abort
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/formulario', methods=['POST', 'GET'])
+def form():
+    return redirect(url_for('index'))
+    # print(request.form)
+    # print(request.form['llave1'])
+    # return request.form['llave2']
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/aborto', methods=['GET', 'POST'])
+def aborto():
+    abort(401)
+
+@app.route('/json', methods=['GET', 'POST'])
+def json():
+    return {
+        "user": "wenardo",
+        "pass": "pelao",
+        "suenos": "tener pelo"
+    }
